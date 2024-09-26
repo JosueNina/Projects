0700568563538b6afb6e66c693eaf0130bab8b08
Josue Nina <josue.nina.ponce@gmail.com>
2024-09-26 18:41:20 -0500

    Create platform model, Create DbContext ans Repositories

--- ServiceHub/PlatformService/Program.cs
+++ ServiceHub/PlatformService/Program.cs
@@ -1,9 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseInMemoryDatabase("InMem");
+});
+
+builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 
 var app = builder.Build();
 
@@ -13,6 +23,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+PrepDb.PrepPopulation(app);
 
 app.UseHttpsRedirection();
 app.Run();
